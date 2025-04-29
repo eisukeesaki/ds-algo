50632b7e5aa275cc25cb5f48dc5930ba6f7e78a6
Eisuke Esaki <eisukeesaki@gmail.com>
2025-04-28 18:47:14 -0700

    allocate more precisely

--- problems/leetcode/c/defang_an_ip_address/defang_ip_addr.c
+++ problems/leetcode/c/defang_an_ip_address/defang_ip_addr.c
@@ -2,10 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-// TODO: document functionality for user
-// TODO: analzye and document time and space complexity for user
+// take an ip address and return a defanged version of it
+// preconditions:
+//    adr is ipv4 address
+// time to complete grows linearly with respect to input size
+// space required grows linearly with respect to input size
 char *defangIPaddr(char *address) {
-   char *defanged = malloc(sizeof("xxx[.]xxx[.]xxx[.]xxx"));
+   char *defanged = malloc(strlen(address) + (strlen("[.]") * 3) + 1);
    if (defanged == NULL) {
       return NULL;
    }
@@ -27,10 +30,10 @@ char *defangIPaddr(char *address) {
 }
 
 int main() {
-   // char a[15] = "255.25.2.0";
-   // char expected[17] = "255[.]25[.]2[.]0";
-   char a[15] = "255.255.255.255";
-   char expected[22] = "255[.]255[.]255[.]255";
+   char a[15] = "255.25.2.0";
+   char expected[17] = "255[.]25[.]2[.]0";
+   // char a[15] = "255.255.255.255";
+   // char expected[22] = "255[.]255[.]255[.]255";
 
    char *d = defangIPaddr(a);
 
