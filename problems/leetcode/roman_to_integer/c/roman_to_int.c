01c50d7c5d0b82897f4e39d386fefe24879c47a8
Eisuke Esaki <eisukeesaki@gmail.com>
2025-05-02 17:17:21 -0700

    simplify algorithm 'roman to int' implemented in c

--- problems/leetcode/roman_to_integer/c/roman_to_int.c
+++ problems/leetcode/roman_to_integer/c/roman_to_int.c
@@ -8,81 +8,59 @@
 //       rn consists of 'I', 'V', 'X', 'L', 'C', 'D', 'M'
 //       1 <= rn <= 3999
 int roman_to_int(char *rn) {
-   char *frmtd = malloc(strlen(rn) * 3 * sizeof(char));
-   if (frmtd == NULL) {
-      return -1;
-   }
-   char *frmtd_org = frmtd;
+   int n = 0;
 
    for (int i = 0; i < strlen(rn); i++) {
-      if (rn[i] == 'I' && rn[i + 1] == 'V') {
-         *frmtd++ = 'I';
-         *frmtd++ = 'I';
-         *frmtd++ = 'I';
-         *frmtd++ = 'I';
-         i++;
+      if (rn[i] == 'I') {
+         if (rn[i + 1] == 'V') {
+            n += 4;
+            i++;
+            continue;
+         }
+         else if (rn[i + 1] == 'X') {
+            n += 9;
+            i++;
+            continue;
+         }
+         n += 1;
       }
-      else if (rn[i] == 'I' && rn[i + 1] == 'X') {
-         *frmtd++ = 'V';
-         *frmtd++ = 'I';
-         *frmtd++ = 'I';
-         *frmtd++ = 'I';
-         *frmtd++ = 'I';
-         i++;
+      else if (rn[i] == 'X') {
+         if (rn[i + 1] == 'L') {
+            n += 40;
+            i++;
+            continue;
+         }
+         else if (rn[i + 1] == 'C') {
+            n += 90;
+            i++;
+            continue;
+         }
+         n += 10;
       }
-      else if (rn[i] == 'X' && rn[i + 1] == 'L') {
-         *frmtd++ = 'X';
-         *frmtd++ = 'X';
-         *frmtd++ = 'X';
-         *frmtd++ = 'X';
-         i++;
+      else if (rn[i] == 'C') {
+         if (rn[i + 1] == 'D') {
+            n += 400;
+            i++;
+            continue;
+         }
+         else if (rn[i + 1] == 'M') {
+            n += 900;
+            i++;
+            continue;
+         }
+         n += 100;
       }
-      else if (rn[i] == 'X' && rn[i + 1] == 'C') {
-         *frmtd++ = 'L';
-         *frmtd++ = 'X';
-         *frmtd++ = 'X';
-         *frmtd++ = 'X';
-         *frmtd++ = 'X';
-         i++;
+      else if (rn[i] == 'V') {
+         n += 5;
       }
-      else if (rn[i] == 'C' && rn[i + 1] == 'D') {
-         *frmtd++ = 'C';
-         *frmtd++ = 'C';
-         *frmtd++ = 'C';
-         *frmtd++ = 'C';
-         i++;
+      else if (rn[i] == 'L') {
+         n += 50;
       }
-      else if (rn[i] == 'C' && rn[i + 1] == 'M') {
-         *frmtd++ = 'D';
-         *frmtd++ = 'C';
-         *frmtd++ = 'C';
-         *frmtd++ = 'C';
-         *frmtd++ = 'C';
-         i++;
+      else if (rn[i] == 'D') {
+         n += 500;
       }
-      else {
-         *frmtd++ = rn[i];
-      }
-   }
-   *frmtd++ = '\0';
-
-   int n = 0;
-   for (int i = 0; i < strlen(frmtd_org); i++) {
-      switch (frmtd_org[i]) {
-         case 'I':
-            n += 1; break;
-         case 'V':
-            n += 5; break;
-         case 'X':
-            n += 10; break;
-         case 'L':
-            n += 50; break;
-         case 'C':
-            n += 100; break;
-         case 'D':
-            n += 500; break;
-         case 'M':
-            n += 1000; break;
+      else if (rn[i] == 'M') {
+         n += 1000;
       }
    }
 
